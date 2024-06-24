97d81a3341dfde922f15c5350f605488b45ec9f4
Nerumi0 <168439850+Nerumi0@users.noreply.github.com>
2024-06-24 16:18:47 +0700

    Add files via upload

---
+++ lab4-5.py
@@ -0,0 +1,12 @@
+#ฟังก์ชั่นหาพื่นที่สามเหลี่ยม = 1/2*b*h
+def triangle(base, height):
+    #print("พื้นที่สามเหลี่ยม %.3f"  % area)
+    return area
+
+#triangle(2,3)
+b = int(input("ค่าฐาน:"))
+h = int(input("ค่าสูง:"))
+
+
+print("พื้นที่สามเหลี่ยม %.3f"  % triangle(b,h))
+    
---
+++ lab4-6.py
@@ -0,0 +1,6 @@
+def circle(radius):
+    area = 22/7*(radius*radius)
+    return area
+R = int(input("ค่ารัศมี:"))
+
+print("พื้นที่วงกลม %.3f"  % circle(R))
